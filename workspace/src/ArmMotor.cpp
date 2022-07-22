4f30c25889f78fa08ea1cda02b01cb7ee314f86f
kumicho93 <taniguchi.takumi@gmail.com>
2022-07-22 10:11:17 +0900

    makefile

--- workspace/src/ArmMotor.cpp
+++ workspace/src/ArmMotor.cpp
@@ -1,9 +1,13 @@
 #include "../include/system.h"
 #include "../include/ArmMotor.h"
+#include "../include/Motor.h"
 
 ArmMotor::ArmMotor()
 {
-    ev3_motor_config( static_cast<motor_port_t>(arm_motor), type );
+    portNum = EV3_PORT_C;
+	ev3_motor_config( static_cast<motor_port_t>(portNum), LARGE_MOTOR );
+    ArmMotor( const ArmMotor &x );
+	ArmMotor&operator=( const ArmMotor & ) { return *this; };
 }
 
 ArmMotor::~ArmMotor()
